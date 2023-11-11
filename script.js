799eb959bf5f1e1d8edca39c0f7009726f049854
jonatasperaza <jonatassilvaperaza@gmail.com>
2023-11-11 11:07:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function clq1() {
+    alert("Voce clicou no botão 1")
+}
+
+let bt2 = document.getElementById("bt2")
+bt2.addEventListener("click", clq2)
+function clq2() {
+    alert("Voce clicou no botão 2")
+}
+let valor1 = document.querySelector("#valor1")
+let valor2 = document.querySelector("#valor2")
+let resultado = document.querySelector("#resultado")
+
+valor1.addEventListener("keyup", function(){
+    somar(Number(valor1.value), Number(valor2.value))
+})
+
+valor2.addEventListener("keyup", function(){
+    somar(Number(valor2.value), Number(valor1.value))
+})
+
+function somar(a, b) {
+    let soma = a + b
+    resultado.innerHTML = soma
+}
